b3b0dfbc131f28ea957e12e0ac2f7a1cdb19fec5
zhangjinlong46 <zhangjinlong46@jd.com>
2022-12-10 18:34:18 +0800

    learn fn and if

--- src/fn_learn/fn_learn.rs
+++ src/fn_learn/fn_learn.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 pub fn fn1() -> i32 {
     5
 }
@@ -9,5 +11,14 @@ pub fn fn2(x:i32) -> i32 {
     } else {
         println!("小于5");
         2
+    }
+}
+
+pub fn fn3(x:i32) {
+    let num = 5;
+    match  x.cmp(&num) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("======"),
+        Ordering::Greater => println!("Too Big"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod game;
 mod fn_learn;
 use fn_learn::fn_learn::fn1;
 use fn_learn::fn_learn::fn2;
+use fn_learn::fn_learn::fn3;
 
 fn main()  {
     let u1 = User::new_user(String::from("tom"), 5);
@@ -13,8 +14,12 @@ fn main()  {
     // guess_game()
     let test =fn1();
     println!("test: {}",test);
+    let  condition = true;
+    // let 使用if表达式
+    let num = if condition{ 5 } else { 6 };
     // 变量隐藏
-    let test = fn2(5);
-    println!("test: {}",test)
+    let test = fn2(num);
+    println!("test: {}",test);
+    fn3(num);
 }
 
